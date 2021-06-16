daf75368ee080a3ee9608ad4e783155706ea8796
BuiDucAnh68 <72397861+BuiDucAnh68@users.noreply.github.com>
2021-06-17 00:17:20 +0700

    Add files via upload

--- SMTP/SMTPClient.Designer.cs
+++ SMTP/SMTPClient.Designer.cs
@@ -29,6 +29,7 @@ namespace SMTP
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SMTPClient));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,127 +42,210 @@ namespace SMTP
             this.Password = new System.Windows.Forms.Label();
             this.txt_password = new System.Windows.Forms.TextBox();
             this.checkBox_Show_Hide = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_attachment = new System.Windows.Forms.Button();
+            this.rTB_File = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rTB_BoxGet = new System.Windows.Forms.RichTextBox();
+            this.rTB_BoxSend = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(-1, 1);
+            this.label1.Font = new System.Drawing.Font("Constantia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 114);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.Size = new System.Drawing.Size(72, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "From";
+            this.label1.Text = "From:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(-1, 47);
+            this.label2.Font = new System.Drawing.Font("Constantia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 161);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.Size = new System.Drawing.Size(42, 26);
             this.label2.TabIndex = 1;
-            this.label2.Text = "To";
+            this.label2.Text = "To:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(-1, 91);
+            this.label3.Font = new System.Drawing.Font("Constantia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 208);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.Size = new System.Drawing.Size(91, 26);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Subject";
+            this.label3.Text = "Subject:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(-1, 134);
+            this.label4.Font = new System.Drawing.Font("Constantia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 255);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.Size = new System.Drawing.Size(69, 26);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Message";
+            this.label4.Text = "Body:";
             // 
             // txt_from
             // 
-            this.txt_from.Location = new System.Drawing.Point(2, 17);
-            this.txt_from.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_from.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_from.Location = new System.Drawing.Point(122, 116);
+            this.txt_from.Margin = new System.Windows.Forms.Padding(2);
             this.txt_from.Name = "txt_from";
-            this.txt_from.Size = new System.Drawing.Size(206, 20);
+            this.txt_from.Size = new System.Drawing.Size(353, 29);
             this.txt_from.TabIndex = 4;
-            this.txt_from.TextChanged += new System.EventHandler(this.txt_from_TextChanged);
             // 
             // txt_To
             // 
-            this.txt_To.Location = new System.Drawing.Point(2, 63);
-            this.txt_To.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_To.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_To.Location = new System.Drawing.Point(122, 159);
+            this.txt_To.Margin = new System.Windows.Forms.Padding(2);
             this.txt_To.Name = "txt_To";
-            this.txt_To.Size = new System.Drawing.Size(561, 20);
+            this.txt_To.Size = new System.Drawing.Size(353, 29);
             this.txt_To.TabIndex = 5;
             // 
             // txt_subject
             // 
-            this.txt_subject.Location = new System.Drawing.Point(2, 106);
-            this.txt_subject.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_subject.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_subject.Location = new System.Drawing.Point(122, 206);
+            this.txt_subject.Margin = new System.Windows.Forms.Padding(2);
             this.txt_subject.Name = "txt_subject";
-            this.txt_subject.Size = new System.Drawing.Size(561, 20);
+            this.txt_subject.Size = new System.Drawing.Size(759, 29);
             this.txt_subject.TabIndex = 6;
             // 
             // richTB_message
             // 
-            this.richTB_message.Location = new System.Drawing.Point(2, 159);
-            this.richTB_message.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTB_message.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTB_message.Location = new System.Drawing.Point(122, 255);
+            this.richTB_message.Margin = new System.Windows.Forms.Padding(2);
             this.richTB_message.Name = "richTB_message";
-            this.richTB_message.Size = new System.Drawing.Size(766, 310);
+            this.richTB_message.Size = new System.Drawing.Size(759, 480);
             this.richTB_message.TabIndex = 7;
             this.richTB_message.Text = "";
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(568, 1);
-            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSend.Font = new System.Drawing.Font("Constantia", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(222, 779);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(200, 146);
+            this.btnSend.Size = new System.Drawing.Size(527, 81);
             this.btnSend.TabIndex = 8;
-            this.btnSend.Text = "Send(SMTPClient)";
+            this.btnSend.Text = "SEND";
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // Password
             // 
             this.Password.AutoSize = true;
-            this.Password.Location = new System.Drawing.Point(255, 0);
+            this.Password.Font = new System.Drawing.Font("Constantia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(505, 117);
             this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(53, 13);
+            this.Password.Size = new System.Drawing.Size(112, 26);
             this.Password.TabIndex = 9;
-            this.Password.Text = "Password";
+            this.Password.Text = "Password:";
             // 
             // txt_password
             // 
-            this.txt_password.Location = new System.Drawing.Point(257, 17);
+            this.txt_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_password.Location = new System.Drawing.Point(623, 115);
             this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(231, 20);
+            this.txt_password.Size = new System.Drawing.Size(231, 29);
             this.txt_password.TabIndex = 10;
             this.txt_password.UseSystemPasswordChar = true;
             // 
             // checkBox_Show_Hide
             // 
             this.checkBox_Show_Hide.AutoSize = true;
-            this.checkBox_Show_Hide.Location = new System.Drawing.Point(500, 17);
-            this.checkBox_Show_Hide.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox_Show_Hide.Location = new System.Drawing.Point(750, 149);
+            this.checkBox_Show_Hide.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox_Show_Hide.Name = "checkBox_Show_Hide";
-            this.checkBox_Show_Hide.Size = new System.Drawing.Size(53, 17);
+            this.checkBox_Show_Hide.Size = new System.Drawing.Size(101, 17);
             this.checkBox_Show_Hide.TabIndex = 11;
-            this.checkBox_Show_Hide.Text = "Show";
+            this.checkBox_Show_Hide.Text = "Show password";
             this.checkBox_Show_Hide.UseVisualStyleBackColor = true;
             this.checkBox_Show_Hide.CheckedChanged += new System.EventHandler(this.checkBox_Show_Hide_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Constantia", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(198, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(574, 59);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Sending Gmail Mail App";
+            // 
+            // btn_attachment
+            // 
+            this.btn_attachment.Location = new System.Drawing.Point(913, 799);
+            this.btn_attachment.Name = "btn_attachment";
+            this.btn_attachment.Size = new System.Drawing.Size(391, 56);
+            this.btn_attachment.TabIndex = 17;
+            this.btn_attachment.Text = "Attachment";
+            this.btn_attachment.UseVisualStyleBackColor = true;
+            this.btn_attachment.Click += new System.EventHandler(this.btn_attachment_Click);
+            // 
+            // rTB_File
+            // 
+            this.rTB_File.BackColor = System.Drawing.SystemColors.GrayText;
+            this.rTB_File.Location = new System.Drawing.Point(913, 638);
+            this.rTB_File.Name = "rTB_File";
+            this.rTB_File.ReadOnly = true;
+            this.rTB_File.Size = new System.Drawing.Size(391, 146);
+            this.rTB_File.TabIndex = 19;
+            this.rTB_File.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(778, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rTB_BoxGet
+            // 
+            this.rTB_BoxGet.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.rTB_BoxGet.Location = new System.Drawing.Point(913, 12);
+            this.rTB_BoxGet.Name = "rTB_BoxGet";
+            this.rTB_BoxGet.ReadOnly = true;
+            this.rTB_BoxGet.Size = new System.Drawing.Size(391, 305);
+            this.rTB_BoxGet.TabIndex = 21;
+            this.rTB_BoxGet.Text = "";
+            // 
+            // rTB_BoxSend
+            // 
+            this.rTB_BoxSend.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.rTB_BoxSend.Location = new System.Drawing.Point(913, 323);
+            this.rTB_BoxSend.Name = "rTB_BoxSend";
+            this.rTB_BoxSend.ReadOnly = true;
+            this.rTB_BoxSend.Size = new System.Drawing.Size(391, 301);
+            this.rTB_BoxSend.TabIndex = 22;
+            this.rTB_BoxSend.Text = "";
             // 
             // SMTPClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(767, 470);
+            this.ClientSize = new System.Drawing.Size(1316, 877);
+            this.Controls.Add(this.rTB_BoxSend);
+            this.Controls.Add(this.rTB_BoxGet);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rTB_File);
+            this.Controls.Add(this.btn_attachment);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.checkBox_Show_Hide);
             this.Controls.Add(this.txt_password);
             this.Controls.Add(this.Password);
@@ -174,9 +258,10 @@ namespace SMTP
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "SMTPClient";
             this.Text = "SMTP";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,6 +281,12 @@ namespace SMTP
         public System.Windows.Forms.Label Password;
         private System.Windows.Forms.TextBox txt_password;
         private System.Windows.Forms.CheckBox checkBox_Show_Hide;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_attachment;
+        private System.Windows.Forms.RichTextBox rTB_File;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox rTB_BoxGet;
+        private System.Windows.Forms.RichTextBox rTB_BoxSend;
     }
 }
 
--- SMTP/SMTPClient.cs
+++ SMTP/SMTPClient.cs
@@ -12,26 +12,183 @@ using System.Net.Mail;
 using System.Net.Sockets;
 using System.Threading;
 using System.IO;
-
+using System.Net.Security;
 namespace SMTP
 {
     public partial class SMTPClient : Form
     {
-        
-        
+        TcpClient tcpClient;
+        StreamReader sr;
+        StreamWriter sw;
+        Thread listenThread;
         public SMTPClient()
         {
             InitializeComponent();
         }
+
+        private void Client(string input)
+        {
+            string text = "C:" + input + "\n";
+            rTB_BoxGet.Text += text;
+        }
+        private void Server(string input)
+        {
+            string text = "S: " + input + "\n";
+            rTB_BoxSend.Text += text;
+        }
+        private string StringBase64(string input)
+        {
+            byte[] inputByte = ASCIIEncoding.ASCII.GetBytes(input);
+            string encodeinput = Convert.ToBase64String(inputByte);
+            return encodeinput;
+        }
+        
+        private void AuthenRespond()
+        {
+            while(true)
+            {
+                string mess = "";
+                mess = sr.ReadLine();
+                if(string.IsNullOrEmpty(mess))
+                {
+                    tcpClient.Close();
+                    break;
+
+                }
+                Server(mess);
+                
+            }
+        }
+        
+        private void AuthenciationMessage()
+        {
+            rTB_BoxGet.Clear();
+            rTB_BoxSend.Clear();
+            tcpClient = new TcpClient();
+            string host = "smtp.gmail.com";
+            tcpClient.Connect(host, 465);//2 port để kết nối với GMAIL 465,587 và đây em dùng port 465 cho TCP Client để không đụng port của SMTP client
+            //Khởi động Ssl để có thể xác nhận cho Client thực hiện việc accept của Server
+            SslStream ssl = new SslStream(tcpClient.GetStream());
+            ssl.AuthenticateAsClient("smtp.gmail.com");
+            sr = new StreamReader(ssl);
+            sw = new StreamWriter(ssl);
+            //Lấy đuôi sau @ của tài khoản gửi và nhận để lấy domain của EHLO  
+            int index = txt_from.Text.Trim().IndexOf('@');
+            string domain = txt_from.Text.Trim().Substring(index + 1);
+            //Bắt đầu lắng nghe Server 
+            CheckForIllegalCrossThreadCalls = false;
+            listenThread = new Thread(new ThreadStart(AuthenRespond));
+            listenThread.IsBackground = true;
+            listenThread.Start();
+            try
+            {
+                //Chạy từng câu lệnh để Client bắt đầu trả về theo từng dòng lệnh để gửi đến Server đã bắt đầu gửi vào richTextBox
+                string data = "EHLO " + domain;
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                data = "AUTH LOGIN";
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                data = StringBase64(txt_from.Text);
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                data = StringBase64(txt_password.Text);
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                data = "MAIL FROM:<" + txt_from.Text + ">";
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                data = "RCPT TO:<" + txt_To.Text + ">";
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                data = "DATA";
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                data = "Subject: " + txt_subject.Text;
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                data = "FROM:<" + txt_from.Text + ">";
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                data = "TO:<" + txt_To.Text + ">";
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                Client("");
+                sw.WriteLine("");
+                sw.Flush();
+
+                data = richTB_message.Text;
+                //Nếu Body chứa xuống dòng thì sẽ nhảy xuống một cái C: để in ra message đó còn nếu không thì chỉ hiện trên một dòng
+                if (data.Contains("\n"))
+                {
+                    
+                    string[] newData = data.Split('\n');
+                    foreach (string stringLine in newData)
+                    {
+                        Client(stringLine);
+                        sw.WriteLine(stringLine);
+                        sw.Flush();
+                    }
+                }
+                else
+                {
+                    Client(data);
+                    sw.WriteLine(data);
+                    sw.Flush();
+                }
+
+                
+
+                Client(".");
+                sw.WriteLine(".");
+                sw.Flush();
+
+                data = "QUIT";
+                Client(data);
+                sw.WriteLine(data);
+                sw.Flush();
+
+                
+            } 
+            
+            
+            catch(Exception ex)
+            {
+                MessageBox.Show("Xuất hiện lỗi" + ex.ToString() + "Vui lòng thử lại");
+            }
+        }
         private void Connect_Gmail()
         {
-            
+
             string mailfrom = txt_from.Text.ToString().Trim();
             string mailto = txt_To.Text.ToString().Trim();
             string password = txt_password.Text.ToString().Trim();
             string subject = txt_subject.Text.ToString().Trim();
-            string body = richTB_message.Text.ToString().Trim();
-            SmtpClient client = new SmtpClient {
+            string body = richTB_message.Text.ToString();
+            
+            
+            SmtpClient client = new SmtpClient
+            {
                 Host = "smtp.gmail.com",
                 Port = 587,
                 EnableSsl = true,
@@ -39,34 +196,54 @@ namespace SMTP
                 UseDefaultCredentials = false,
                 Credentials = new NetworkCredential(mailfrom, password),
             };
-            using (var message = new MailMessage(mailfrom, mailto)
-            {
-                Subject = subject,
-                Body = body
-            })
-            {
-                client.Send(message);
-            }
+
             
-
-
-
+            using (MailMessage message = new MailMessage())
+            { 
+                message.From = new MailAddress(mailfrom);
+                message.Subject = subject;
+                message.Body = body;
+                message.IsBodyHtml = true;
+                message.To.Add(mailto);
+                //Chọn các file cần gửi nếu không gửi file thì không sử dụng phương thức gửi này
+                if (rTB_File.Text != "")
+                {
+                    string[] attachment = rTB_File.Text.Split('\n');
+                    foreach (string attachfile in attachment)
+                    {
+                        if(attachfile != "")
+                            message.Attachments.Add(new Attachment(attachfile));
+                    }
+                    
+                }
+                try
+                {
+                    client.Send(message);
+                    MessageBox.Show("Gửi mail thành công", "Success");
+                    rTB_File.Text = "";
+                    richTB_message.Text = "";
+                    txt_subject.Text = "";
+                }
+                catch
+                {
+                    MessageBox.Show("Gửi mail thất bại", "Error");
+                }
+            }
         }
+        
+           
         private void btnSend_Click(object sender, EventArgs e)
         {
-            try
+            if ((!String.IsNullOrEmpty(txt_from.Text)) && (!String.IsNullOrEmpty(txt_password.Text)) && (!String.IsNullOrEmpty(txt_To.Text)))
             {
+                AuthenciationMessage();
                 Connect_Gmail();
-                MessageBox.Show("Send message to gmail to public","Success");
-            }
-            catch
-            {
-                MessageBox.Show("Failure Message to Gmail", "Error");
-            }
-        }
 
-        private void txt_from_TextChanged(object sender, EventArgs e)
-        {
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin trước khi gửi!", "Lỗi");
+            }    
 
         }
 
@@ -81,11 +258,15 @@ namespace SMTP
                 txt_password.UseSystemPasswordChar = true;
             }
         }
-       
 
-        private void rTB_TCP_TextChanged(object sender, EventArgs e)
+        private void btn_attachment_Click(object sender, EventArgs e)
         {
-
+            //Gửi tối đa là 24 MB cho tổng file đính kèm và hiện tên lên richTextBox
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            rTB_File.Text += ofd.FileName + "\n"; 
         }
+
+       
     }
 }
